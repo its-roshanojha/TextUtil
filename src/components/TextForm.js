0ca7b7fe6b1c578ae22641ea1d1e1d7de05918b5
Roshan <its.roshanojha@gmail.com>
2022-11-14 18:12:46 +0530

    Added features like remove space and copyText in TextUtils

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -21,6 +21,19 @@ export default function TextForm(props) {
         setText(newText);
     }
 
+    const handleCopyText = () => {
+        var text = document.getElementById('myBox');
+        text.select();
+        text.setSelectionRange(0, 99999);
+        navigator.clipboard.writeText(text.value);
+        alert('Your text copied');
+    }
+
+    const handleExtraSpaces = () => {
+        let newText = text.split(/[  ]+/);
+        setText(newText.join(" "))
+    }
+
     
     const [text, setText] = useState('enter text here');
     // wrong way to set the state
@@ -39,6 +52,9 @@ export default function TextForm(props) {
         <button className="btn btn-primary mx-2" onClick={handleUpClick}>Conver to upper case</button>
         <button className="btn btn-primary mx-2" onClick={handleLowClick}>Conver to lower case</button>
         <button className="btn btn-primary mx-2" onClick={handleClearText}>Clear Text</button>
+        <button className="btn btn-primary mx-2" onClick={handleCopyText}>Copy Text</button>
+        <button className="btn btn-primary mx-2" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
+
 
 
         </div>
